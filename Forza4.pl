5e7323e7276ec66e298cb51a08792be8128008eb
Nico <nferrari@studenti.uninsubria.it>
2022-07-22 12:17:09 +0200

    Programma completo e funzionante

--- Forza4.pl
+++ Forza4.pl
@@ -2,6 +2,8 @@ win(Board, Player) :- rowwin(Board, Player).
 win(Board, Player) :- colwin(Board, Player).
 win(Board, Player) :- diagwin(Board, Player).
 
+% Condizioni di vittoria sulle righe
+
 rowwin(Board, Player) :- Board = [Player, Player, Player, Player, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _].
 rowwin(Board, Player) :- Board = [_, Player, Player, Player, Player, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _].
 rowwin(Board, Player) :- Board = [_, _, Player, Player, Player, Player, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _].
@@ -18,6 +20,8 @@ rowwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _
 rowwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, Player, Player, Player, Player, _].
 rowwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, Player, Player, Player, Player].
 
+% Condizioni di vittoria sulle colonne
+
 colwin(Board, Player) :- Board = [Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, _, _, _, _, _, _].
 colwin(Board, Player) :- Board = [_, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _].
 colwin(Board, Player) :- Board = [_, Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, _, _, _, _, _].
@@ -31,13 +35,14 @@ colwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, _, _, Player, _, _, _,
 colwin(Board, Player) :- Board = [_, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, _].
 colwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, Player, _, _, _, _, _, Player].
 
+% Condizioni di vittoria sulle diagonali
+
 diagwin(Board, Player) :- Board = [_, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, _, _, _, _, _, _, _].
 diagwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, _].
 diagwin(Board, Player) :- Board = [_, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, _, _, _, _, _, _].
 diagwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _].
 diagwin(Board, Player) :- Board = [_, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, _, _, _, _, _].
 diagwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _, _, Player, _, _, _].
-
 diagwin(Board, Player) :- Board = [_, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _].
 diagwin(Board, Player) :- Board = [Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, _, _].
 diagwin(Board, Player) :- Board = [_, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _].
@@ -45,28 +50,25 @@ diagwin(Board, Player) :- Board = [_, Player, _, _, _, _, _, _, Player, _, _, _,
 diagwin(Board, Player) :- Board = [_, _, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player].
 diagwin(Board, Player) :- Board = [_, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _, Player, _, _, _, _, _, _].
 
-
-decreaseCounter(Board, [H|T], ColIndex, Counter, Simbolo, QueueList, NewList, NewBoardInsert) :-
+move(Board, [H|T], ColIndex, Counter, Simbolo, QueueList, NewList, NewBoardInsert) :-
     Counter < (ColIndex-1),
     NewCounter is Counter + 1,
     append(QueueList, [H], NewList1), !,
-    decreaseCounter(Board, T, ColIndex, NewCounter, Simbolo, NewList1, NewList, NewBoardInsert), !. 
+    move(Board, T, ColIndex, NewCounter, Simbolo, NewList1, NewList, NewBoardInsert), !. 
 
-decreaseCounter(Board, [H|T], ColIndex, Counter, Simbolo, QueueList, NewList, NewBoardInsert) :-
-    (H is 0 -> ((ColIndex < 6) -> write('Mossa illegale sulla colonna '),
-    write(ColIndex), nl,
+move(Board, [H|T], ColIndex, Counter, Simbolo, QueueList, NewList, NewBoardInsert) :-
+    (H is 0 -> ((ColIndex < 6) -> write('Giocatore '), write(Simbolo), write(' hai fatto una mossa illegale sulla colonna '),
+    write(ColIndex), write(', la tua mossa verrà spostata nella colonna alla tua destra'), nl,
     NewColIndex is ColIndex + 1,
-	decreaseCounter(Board, [H|T], NewColIndex, Counter, Simbolo, QueueList, NewList, NewBoardInsert))).
-
-decreaseCounter(Board, [H|_], ColIndex, _, Simbolo, QueueList, NewList, NewBoardInsert) :-
-    (H is 0 -> ((ColIndex >= 6) -> write('Mossa illegale sulla colonna '),
-    write(ColIndex), nl,
+	move(Board, [H|T], NewColIndex, Counter, Simbolo, QueueList, NewList, NewBoardInsert) ; 
+    write('Giocatore '), write(Simbolo), write(' hai fatto una mossa illegale sulla colonna '),
+    write(ColIndex), write(', la tua mossa verrà spostata nella prima colonna'), nl,
     NewColIndex is 1,
     NewCounter is 0,
     append(QueueList, [H], NewQueueList),
-	decreaseCounter(Board, NewQueueList, NewColIndex, NewCounter, Simbolo, _, NewList, NewBoardInsert))).
+	move(Board, NewQueueList, NewColIndex, NewCounter, Simbolo, _, NewList, NewBoardInsert))).    
 
-decreaseCounter(Board, [H|T], ColIndex, Counter, Simbolo, QueueList, NewList, NewBoardInsert) :-
+move(Board, [H|T], ColIndex, Counter, Simbolo, QueueList, NewList, NewBoardInsert) :-
     Counter is (ColIndex-1),
     N is H-6,
     append(QueueList, [N|T], NewList),
@@ -90,7 +92,7 @@ display([AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP,AQ,AR,AS,AT,AU,AV,AW,AX
 respond(Board, _, _, _) :- win(Board, o), write('Congratulazioni PC, hai vinto!').
 respond(Board, _, _, _) :- win(Board, x), write('Congratulazioni giocatore, hai vinto!').
 respond(Board, Newboard, NewList, NewList1) :- random(1,7, Col_Index),
-	decreaseCounter(Board, NewList, Col_Index, 0, o, _, NewList1, Newboard).
+	move(Board, NewList, Col_Index, 0, o, _, NewList1, Newboard).
 
 explain :-
     write('Giochi come X, scegli una colonna da 1 a 6'),
@@ -99,10 +101,10 @@ explain :-
 
 playfrom(Board, L) :- 
     read(N),
-    decreaseCounter(Board, L, N, 0, x, _, NewList, Newboard),
+    move(Board, L, N, 0, x, _, NewList, Newboard),
     display(Newboard),
     respond(Newboard, NewNewboard, NewList, NewList1),
     display(NewNewboard),
-    playfrom(NewNewboard, NewList1).
+    playfrom(NewNewboard, NewList1)
 
 playo :- explain, L = [30,30,30,30,30,30], playfrom([b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b], L).
